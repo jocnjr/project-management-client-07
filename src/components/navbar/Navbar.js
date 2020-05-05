0953d3b819766808f283cfed33d5cc6f24628137
Jose Octavio de Castro Neves Jr <joc@fullsquad.com.br>
2020-05-05 15:15:21 -0300

    add and list projects. project details on the way

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+// components/navbar/Navbar.js
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="nav-style">
+      <ul>
+        <li>
+          <Link to="/projects" style={{ textDecoration: "none" }}>
+            Projects
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
